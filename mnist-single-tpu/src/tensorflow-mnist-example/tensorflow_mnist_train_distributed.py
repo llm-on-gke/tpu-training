4f471e67e1edd4a869dcae700730f816178729cf
Rick(Rugui) Chen <rickruguichen@google.com>
2025-04-16 16:38:24 -0400

    add mnist tpu example

--- mnist-single-tpu/src/tensorflow-mnist-example/tensorflow_mnist_train_distributed.py
+++ mnist-single-tpu/src/tensorflow-mnist-example/tensorflow_mnist_train_distributed.py
@@ -3,6 +3,12 @@ os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 import tensorflow_datasets as tfds
 import tensorflow as tf
 
+resolver = tf.distribute.cluster_resolver.TPUClusterResolver(tpu='local')
+tf.config.experimental_connect_to_cluster(resolver)
+# This is the TPU initialization code that has to be at the beginning.
+tf.tpu.experimental.initialize_tpu_system(resolver)
+print("All devices: ", tf.config.list_logical_devices('TPU'))
+
 datasets, info = tfds.load(name='mnist', with_info=True, as_supervised=True)
 
 mnist_train, mnist_test = datasets['train'], datasets['test']
