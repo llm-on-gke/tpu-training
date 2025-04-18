01080931efd4cdcf06ec672494624b62e6ff9a42
Rick(Rugui) Chen <rickruguichen@google.com>
2025-04-18 09:35:11 -0400

    add flag to download

--- mnist-single-tpu/src/tensorflow-mnist-example/trainer.sh
+++ mnist-single-tpu/src/tensorflow-mnist-example/trainer.sh
@@ -5,9 +5,10 @@
 
 #pip install https://storage.googleapis.com/tensorflow-public-build-artifacts/prod/tensorflow/official/release/nightly/linux_x86_tpu/wheel_py310/749/20240915-062017/github/tensorflow/build_output/tf_nightly_tpu-2.18.0.dev20240915-cp310-cp310-manylinux_2_17_x86_64.manylinux2014_x86_64.whl -f https://storage.googleapis.com/libtpu-tf-releases/index.html --force
 
-git clone https://github.com/tensorflow/recommenders.git
-
-git clone https://github.com/tensorflow/models.git
+if [ "$install_data" = "true" ]; then
+  git clone https://github.com/tensorflow/recommenders.git
+  git clone https://github.com/tensorflow/models.git
+fi
 
 export PYTHONPATH=~/recommenders/:~/models/
 
